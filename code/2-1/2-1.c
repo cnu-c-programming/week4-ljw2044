f526b41ecfa724b3e37f7f5adb10cb1216c11e9a
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-03-27 20:57:48 +0900

    2-1c

--- code/2-1/2-1.c
+++ code/2-1/2-1.c
@@ -2,12 +2,12 @@
 
 int main() {
     const char* date = "2026-03-24";
-    char buffer[64]; 
+    char buffer[64];
     int year, month, day;
 
-    scanf(date, "%4d-%2d-%2d", &year, &month, &day);
-    sprintf(buffer, "Year: %d, Month: %d, Day: %d", year, month, day);
-    
+    sscanf(date, "%d-%d-%d", &year, &month, &day);
+    sprintf(buffer, "%d/%d/%d", day, month, year);
+
     printf("%s\n", buffer);
     return 0;
 }
