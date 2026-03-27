0f09878b37c3665816411432ee6c8e89f2917501
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-03-27 10:19:50 +0900

    1-2c

--- code/1-2/1-2.c
+++ code/1-2/1-2.c
@@ -3,13 +3,11 @@
 #include "util.h"
 
 int main() {
-    for(int i=0; i<100; i++)  {
-        if (i%2 == 0) {
+    for(int i=0; i<100; i++)
+        if (i%2 == 0) 
             logger("even");
-        } else {
-            logger("odd")
-        }
-    }
+        else
+            logger("odd");
 
     return 0;
 }
